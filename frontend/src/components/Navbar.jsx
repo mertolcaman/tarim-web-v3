7261d39eba8c282a6e7111648ebe63369a4d4d00
Mert OLCAMAN <mertunknown45@gmail.com>
2025-05-10 22:17:30 +0300

    adding gittattributes file and making navbar responsive for mobile users

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,22 @@ const Navbar = ({ onLogout }) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
             <Link className="navbar-brand" to="/">IoT Dashboard</Link>
-            <div className="collapse navbar-collapse">
+
+            {/* Mobile Toggle Button */}
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            {/* Collapsible Content */}
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
                         <Link className="nav-link" to="/">Sensors</Link>
@@ -22,3 +37,4 @@ const Navbar = ({ onLogout }) => {
 };
 
 export default Navbar;
+
