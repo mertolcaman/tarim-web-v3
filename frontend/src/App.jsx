cfdae842386cf0e8365194bffb1737d408a75e24
Mert OLCAMAN <mertunknown45@gmail.com>
2025-06-27 14:33:55 +0300

    adding S3 upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import Navbar from './components/Navbar';
 import ChatPage from './components/chatbot/ChatPage';
 
 import AdvancedDashboard from './pages/AdvancedDashboard';
-import UploadPage from "./pages/UploadPage";
+import Upload from "./pages/UploadPage";
 
 
 function App() {
@@ -31,7 +31,7 @@ function App() {
         <Route path="/devices" element={<DevicePage />} />
         <Route path="/chat" element={<ChatPage />} />
         <Route path="/charts" element={<AdvancedDashboard />} />
-        <Route path='/upload-firmware' element={<UploadPage />} />
+        <Route path='/upload-firmware' element={<Upload />} />
 
       </Routes>
     </Router>
