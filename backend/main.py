b88dba13e6b7994d1d7029c8cd408598bc4b73d3
Mert OLCAMAN <mertunknown45@gmail.com>
2025-07-27 01:07:21 +0300

    testing CI/CD pipeline for backend

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     # allow_origins=["*"],
     allow_origins=[
         "http://localhost:5173",
-        "http://34.229.13.108",
+        "http://34.229.13.10",
     ],
     allow_credentials=True,
     allow_methods=["*"],
