d8da3ca2a92b6c069979e844d5dceb4d895c19bc
Mert OLCAMAN <mertunknown45@gmail.com>
2025-07-29 17:02:00 +0300

    fixing CORS origins

--- backend/main.py
+++ backend/main.py
@@ -11,8 +11,8 @@ app.add_middleware(
     # allow_origins=["*"],
     allow_origins=[
         "http://localhost:5173",
-        "http://34.229.13.108/",
-        "https://13.221.129.38/",
+        "http://34.229.13.108",
+        "http://13.221.129.38",
     ],
     allow_credentials=True,
     allow_methods=["*"],
